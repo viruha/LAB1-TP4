7644c87dd96e492db69ba8401697aa9e53079d93
viruha <maurocastro1@gmail.com>
2022-09-26 11:37:51 -0300

    UML + clases

---
+++ src/tp4/Alumno.java
@@ -0,0 +1,41 @@
+package tp4;
+
+public class Alumno {
+
+    private int legajo;
+
+    private String apellido;
+
+    private String nombre;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+    }
+
+    public int getLegajo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void serLegajo(int legajo) {
+    }
+
+    public String getApellido() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setApellido(String apellido) {
+    }
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setNombre(String nombre) {
+    }
+
+    public void agregarMateria(Materia m) {
+    }
+
+    public int cantidadMaterias() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/tp4/Materia.java
@@ -0,0 +1,34 @@
+package tp4;
+
+public class Materia {
+
+    private int idMateria;
+
+    private String nombre;
+
+    private int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+    }
+
+    public int getIdMateria() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setIdMateria(int idMateria) {
+    }
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setNombre(String nombre) {
+    }
+
+    public int getAnio() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setAnio(int anio) {
+    }
+}
